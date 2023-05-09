c0d17ad043c3b9a381f0ae982126d463bd8f091b
devavanza <dev@avanzainnovations.com>
2023-05-09 18:51:17 +0500

    google sign in initial implementation

--- app/src/main/java/com/hotel/theconvo/MainActivity.kt
+++ app/src/main/java/com/hotel/theconvo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.hotel.theconvo
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +11,19 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hotel.theconvo.destinations.SplashScreenDestination
 import com.hotel.theconvo.presentation.vm.ConvoViewModel
 
@@ -30,6 +45,8 @@ import javax.inject.Inject
     @Inject
     lateinit var sharedViewModel: ConvoViewModel
 
+    lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
      companion object {
        lateinit var loginUseCase: LoginUseCase
@@ -54,6 +71,8 @@ import javax.inject.Inject
         }
 
 
+      //  googleSignIn()
+
         loginUseCase = getUserByIdUseCase
 
        /** lifecycleScope.launch {
@@ -64,6 +83,88 @@ import javax.inject.Inject
         }*/
 
 
+    }
+
+
+    fun googleSignIn() {
+
+        // Initialize sign in options the client-id is copied form google-services.json file
+        //103571255332-sdn1s6aluto804fd3f5dh6oid68hnddn.apps.googleusercontent.com
+       // 103571255332-js1ots0bs0f0pta0frr9itulmekbmk1p.apps.googleusercontent.com
+        firebaseAuth = Firebase.auth
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("103571255332-js1ots0bs0f0pta0frr9itulmekbmk1p.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        // Initialize sign in client
+        googleSignInClient = GoogleSignIn.getClient(this@MainActivity, googleSignInOptions)
+
+        val intent: Intent = googleSignInClient.signInIntent
+        // Start activity for result
+        startActivityForResult(intent, 100)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Check condition
+        if (requestCode == 100) {
+            // When request code is equal to 100 initialize task
+            val signInAccountTask: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            // check condition
+            if (signInAccountTask.isSuccessful) {
+                // When google sign in successful initialize string
+                val s = "Google sign in successful"
+                // Display Toast
+                //displayToast(s)
+                // Initialize sign in account
+                try {
+                    // Initialize sign in account
+                    val googleSignInAccount = signInAccountTask.getResult(ApiException::class.java)
+                    // Check condition
+                    if (googleSignInAccount != null) {
+                        // When sign in account is not equal to null initialize auth credential
+                        val authCredential: AuthCredential = GoogleAuthProvider.getCredential(
+                            googleSignInAccount.idToken, null
+                        )
+                        // Check credential
+                        firebaseAuth.signInWithCredential(authCredential)
+                            .addOnCompleteListener(this) { task ->
+                                // Check condition
+                                if (task.isSuccessful) {
+                                    // When task is successful redirect to profile activity
+                                   /** startActivity(
+                                        Intent(
+                                            this@MainActivity,
+                                            ProfileActivity::class.java
+                                        ).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                    )*/
+                                    Log.i("Task Success full", "Success")
+                                    // Display Toast
+                                    //displayToast("Firebase authentication successful")
+                                } else {
+                                    // When task is unsuccessful display Toast
+                                    //displayToast(
+                                      //  "Authentication Failed :" + task.exception.message
+                                    //)
+
+                                    Log.i("Authentication Failed:","Authentication Failed")
+                                }
+                            }
+                    }
+                } catch (e: ApiException) {
+                    e.printStackTrace()
+                }
+            }
+            else {
+                //Toast.makeText(LocalContext.current,"Google sign in unsuccessful",Toast.LENGTH_LONG).show()
+                Log.i("Unsuccess","Unsuccess")
+            }
+        }
     }
 
 
